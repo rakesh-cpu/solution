6b3739528e14185f239fa99f2059de3dfed7b060
Rakesh dontula <52991653+rakesh-cpu@users.noreply.github.com>
2024-01-22 18:08:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,7 @@ app.use("/api/user", async (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/", (req, res) => {
-    res.send("Welcome to the backend");
+    res.send("Welcome to the backend 🎉🤞");
 });
 
 const port = process.env.PORT || 8080;
